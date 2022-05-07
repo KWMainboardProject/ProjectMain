3307b9975e53d2f5032c4dc25eff21a701889ae6
SonUJ97 <65939754+SonUJ97@users.noreply.github.com>
2022-05-07 20:16:52 +0900

    테스트폼 구현
    사진 입력 + 텍스트 정렬

--- webPage/js/scripts.js
+++ webPage/js/scripts.js
@@ -5,3 +5,148 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+function previewImage(targetObj, View_area) {
+	var preview = document.getElementById(View_area); //div id
+	var ua = window.navigator.userAgent;
+
+  //ie일때(IE8 이하에서만 작동)
+	if (ua.indexOf("MSIE") > -1) {
+		targetObj.select();
+		try {
+			var src = document.selection.createRange().text; // get file full path(IE9, IE10에서 사용 불가)
+			var ie_preview_error = document.getElementById("ie_preview_error_" + View_area);
+
+
+			if (ie_preview_error) {
+				preview.removeChild(ie_preview_error); //error가 있으면 delete
+			}
+
+			var img = document.getElementById(View_area); //이미지가 뿌려질 곳
+
+			//이미지 로딩, sizingMethod는 div에 맞춰서 사이즈를 자동조절 하는 역할
+			img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='scale')";
+		} catch (e) {
+			if (!document.getElementById("ie_preview_error_" + View_area)) {
+				var info = document.createElement("<p>");
+				info.id = "ie_preview_error_" + View_area;
+				info.innerHTML = e.name;
+				preview.insertBefore(info, null);
+			}
+		}
+  //ie가 아닐때(크롬, 사파리, FF)
+	} else {
+		var files = targetObj.files;
+		for ( var i = 0; i < files.length; i++) {
+			var file = files[i];
+			var imageType = /image.*/; //이미지 파일일경우만.. 뿌려준다.
+			if (!file.type.match(imageType))
+				continue;
+			var prevImg = document.getElementById("prev_" + View_area); //이전에 미리보기가 있다면 삭제
+			if (prevImg) {
+				preview.removeChild(prevImg);
+			}
+			var img = document.createElement("img"); 
+			img.id = "prev_" + View_area;
+			img.classList.add("obj");
+			img.file = file;
+			img.style.width = '400px'; 
+			img.style.height = '400px';
+			preview.appendChild(img);
+			if (window.FileReader) { // FireFox, Chrome, Opera 확인.
+				var reader = new FileReader();
+				reader.onloadend = (function(aImg) {
+					return function(e) {
+						aImg.src = e.target.result;
+					};
+				})(img);
+				reader.readAsDataURL(file);
+			} else { // safari is not supported FileReader
+				//alert('not supported FileReader');
+				if (!document.getElementById("sfr_preview_error_"
+						+ View_area)) {
+					var info = document.createElement("p");
+					info.id = "sfr_preview_error_" + View_area;
+					info.innerHTML = "not supported FileReader";
+					preview.insertBefore(info, null);
+				}
+			}
+		}
+	}
+}
+function DropFile(dropAreaId, fileListId) {
+    let dropArea = document.getElementById(dropAreaId);
+    let fileList = document.getElementById(fileListId);
+  
+    function preventDefaults(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  
+    function highlight(e) {
+      preventDefaults(e);
+      dropArea.classList.add("highlight");
+    }
+  
+    function unhighlight(e) {
+      preventDefaults(e);
+      dropArea.classList.remove("highlight");
+    }
+  
+    function handleDrop(e) {
+      unhighlight(e);
+      let dt = e.dataTransfer;
+      let files = dt.files;
+  
+      handleFiles(files);
+  
+      const fileList = document.getElementById(fileListId);
+      if (fileList) {
+        fileList.scrollTo({ top: fileList.scrollHeight });
+      }
+    }
+  
+    function handleFiles(files) {
+      files = [...files];
+      files.forEach(previewFile);
+    }
+  
+    function previewFile(file) {
+      console.log(file);
+      fileList.appendChild(renderFile(file));
+    }
+  
+    function renderFile(file) {
+      let fileDOM = document.createElement("div");
+      fileDOM.className = "file";
+      fileDOM.innerHTML = `
+        <div class="thumbnail">
+          <img src="https://img.icons8.com/pastel-glyph/2x/image-file.png" alt="파일타입 이미지" class="image">
+        </div>
+        <div class="details">
+          <header class="header">
+            <span class="name">${file.name}</span>
+            <span class="size">${file.size}</span>
+          </header>
+          <div class="progress">
+            <div class="bar"></div>
+          </div>
+          <div class="status">
+            <span class="percent">100% done</span>
+            <span class="speed">90KB/sec</span>
+          </div>
+        </div>
+      `;
+      return fileDOM;
+    }
+  
+    dropArea.addEventListener("dragenter", highlight, false);
+    dropArea.addEventListener("dragover", highlight, false);
+    dropArea.addEventListener("dragleave", unhighlight, false);
+    dropArea.addEventListener("drop", handleDrop, false);
+  
+    return {
+      handleFiles
+    };
+  }
+  
+  const dropFile = new DropFile("drop-file", "files");
