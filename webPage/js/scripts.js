964f1ec66da4f30badb05add7f702e0f2ce1edf2
SonUJ97 <65939754+SonUJ97@users.noreply.github.com>
2022-05-11 13:15:18 +0900

    임시
    곧수정

--- webPage/js/scripts.js
+++ webPage/js/scripts.js
@@ -150,3 +150,23 @@ function DropFile(dropAreaId, fileListId) {
   }
   
   const dropFile = new DropFile("drop-file", "files");
+
+  // 로그인 구현 js
+  <script src="./js/login.js"></script>
+
+  function check_input()
+  {
+    if(!document.loginSbmt.id.value)
+    {
+      alert("아이디를 입력하세요");
+      document.loginSbmt.id.focus();
+      return;
+    }
+
+    if(!document.loginSbmt.pass.value){
+      alert("비밀번호를 입력하세요");
+      document.loginSbmt.pass.focus();
+      return;
+    }
+    document.loginSbmt.submit();
+  }
