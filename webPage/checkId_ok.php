334fffc4dc9b76004464f925a40eaf91a8d0cf7b
SonUJ97 <65939754+SonUJ97@users.noreply.github.com>
2022-05-16 00:10:32 +0900

    DB 연동 완료
    테스트는 도메인에서

--- webPage/checkId_ok.php
+++ webPage/checkId_ok.php
@@ -1,10 +1,10 @@
 <?php
     include 'config/db_connection.php';
 
-    $userid = $_GET['PID'];
-    $sql = "select useridx from tb_user where userid='$userid'";
+    $userid = $_GET['userid'];
+    $sql = "select PID from users where PID='$userid'";
     $result = mysqli_query($conn, $sql);
     $data = mysqli_fetch_array($result);
 
-    echo isset($data['useridx']) ? "X" : "O";
+    echo isset($data['PID']) ? "X" : "O";
 ?>
