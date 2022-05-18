6c54be4bb0f6ce0b1f1426861b61c1a7faf3ae3d
taktakgyun <vbmrkk@naver.com>
2022-05-18 13:51:10 +0900

    Create WorkFactory & WorkStrategy

--- c#/RequestTaskProcessing/TaskWorkClass.cs
+++ c#/RequestTaskProcessing/TaskWorkClass.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace RequestTaskProcessing
 {
-    class TaskWorkClass
+
+    abstract class WorkManager : IMessageConsumeAble
     {
+
+
+        public WorkManager()
+        {
+            productor.SetQueue(q);
+        }
+
+        public TaskMessage Consume()
+        {
+            TaskMessage message = new TaskMessage();
+            if (q.IsEmpty) return message;
+
+            q.TryDequeue(out message);
+            return message;
+        }
+
+        public IMessageProductAble GetProductor()
+        {
+            return productor;
+        }
+
+        public bool IsEmpty()
+        {
+            return q.IsEmpty;
+        }
+
+        abstract protected void Run();
+
+        protected ConcurrentQueue<TaskMessage> q = new ConcurrentQueue<TaskMessage>();
+        protected SimpleMessageProductor productor = new SimpleMessageProductor();
     }
 }
