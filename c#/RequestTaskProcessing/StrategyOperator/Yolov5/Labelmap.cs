ce4e6eab899c56ea03f69d7dd8673bc0866768f4
taktakgyun <vbmrkk@naver.com>
2022-05-25 16:12:20 +0900

    Add YOLOv5

---
+++ c#/RequestTaskProcessing/StrategyOperator/Yolov5/Labelmap.cs
@@ -0,0 +1,8 @@
+﻿namespace OnnxSample.Yolov5
+{
+    public class LabelMap
+    {
+        //public static readonly string[] Labels = new[] { "person", "car", "animal", "bus", "fire" };
+        public static readonly string[] Labels = new[] { "Overall", "Bottom", "Top", "Outer", "Shoes" };
+    }
+}
