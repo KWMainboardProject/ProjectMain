87da9c4910bf12d3f20a030e58489431e242c719
taktakgyun <vbmrkk@naver.com>
2022-05-26 08:50:04 +0900

    taskMessage

---
+++ c#/RequestTaskProcessing/StrategyOperator/SubCategory/BottomClassification.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RequestTaskProcessing.StrategyOperator;
+
+namespace RequestTaskProcessing.StrategyOperator.SubCategory
+{
+    class BottomClassification : IStrategyOperateAble
+    {
+        public void ClearResource()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskMessage GetMessage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetResource(TaskMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ c#/RequestTaskProcessing/StrategyOperator/SubCategory/OverallClassification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestTaskProcessing.StrategyOperator.SubCategory
+{
+    class OverallClassification
+    {
+    }
+}
